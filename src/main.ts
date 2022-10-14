1488318d8e23ef9dfd3493c006f782bed74b1fb6
DaveFlashNL <72280178+DaveFlashNL@users.noreply.github.com>
2022-10-14 15:33:02 +0200

    Clipboard-a-palooza! branch-test for pull request
    Electron WMD now with an Edit menu and clipboard exchange between the application and the system, now you can enjoy copy, paste, cut, SelectAll, redo, undo on all text to and from your system and EWMD, to enjoy enhanced editing capabilities when titling all MD's!

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,8 @@ async function createWindow() {
 
     const setupMenu = () =>
         Menu.setApplicationMenu(
-            Menu.buildFromTemplate([
-                {
+            Menu.buildFromTemplate(
+                [{
                     label: 'File',
                     submenu: [
                         {
@@ -61,8 +61,37 @@ async function createWindow() {
                             label: 'Exit',
                             click: () => window.close(),
                         },
-                    ],
-                },
+                ]}, {
+                    label: 'Edit',
+                    submenu: [
+                        {   label: "Undo", 
+                            accelerator: "CmdOrCtrl+Z", 
+                            role: "undo", 
+                        },
+                        {   label: "Redo",
+                            accelerator: "Shift+CmdOrCtrl+Z",
+                            role: "redo", 
+                        },
+                        {
+                            type: "separator", 
+                        },
+                        {   label: "Cut",
+                            accelerator: "CmdOrCtrl+X",
+                            role: "cut",
+                        },
+                        {   label: "Copy",
+                            accelerator: "CmdOrCtrl+C",
+                            role: "copy",
+                        },
+                        {   label: "Paste",
+                            accelerator: "CmdOrCtrl+V",
+                            role: "paste",
+                        },
+                        {   label: "Select All",
+                            accelerator: "CmdOrCtrl+A",
+                            role: "selectAll",
+                        },
+                    ]},
             ])
         );
 
